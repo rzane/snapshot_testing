38cd610d9eb4e994f866f3cca5fdbcb01639d78b
Ray Zane <raymondzane@gmail.com>
2019-07-06 14:29:25 -0400

    Add a test for custom serializers

--- spec/snapshot_testing/snapshot_spec.rb
+++ spec/snapshot_testing/snapshot_spec.rb
@@ -1,4 +1,21 @@
 RSpec.describe SnapshotTesting::Snapshot do
+  class Jawn
+    attr_reader :value
+    def initialize(value)
+      @value = value
+    end
+  end
+
+  class JawnSerializer
+    def accepts?(object)
+      object.is_a? Jawn
+    end
+
+    def dump(jawn)
+      jawn.value
+    end
+  end
+
   let(:data) {
     { "hello" => "world", "foo" => "bar\nbaz" }
   }
@@ -22,5 +39,17 @@ RSpec.describe SnapshotTesting::Snapshot do
 
   it "dumps snapshots" do
     expect(SnapshotTesting::Snapshot.dump(data)).to eq(snapshot)
+  end
+
+  it "allows the use of custom serializers" do
+    SnapshotTesting::Snapshot.use(JawnSerializer.new)
+
+    snapshot = SnapshotTesting::Snapshot.dump("jawn" => Jawn.new("jint"))
+
+    expect(snapshot).to eq(<<~EOS)
+    snapshots["jawn"] = <<~SNAP
+    jint
+    SNAP
+    EOS
   end
 end
